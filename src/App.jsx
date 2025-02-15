9932e4db194a4c16866af17daaeb98ee3a9cf8dd
Lawri Darbyshire <166262530+Darbyshire64@users.noreply.github.com>
2025-02-15 10:32:35 +0000

    Updated App.jsx, About.jsx, Home.jsx and added main1.jpeg

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "St Barts FTC Robotics Team",
+  title: "PORTFOLIO",
+  email: "example@example.com",
+  gitHub: "stbartsftc",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,12 +31,12 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "3D Printing",
+  "Design & Critical Thinking",
+  "Programmnig",
+  "Troubleshooting",
+  "",
+  "",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Placeholder";
 
 const About = () => {
   return (
